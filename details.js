e96fa627c4f25582a81aee42709cb083c7d4fed6
Davi Quinino <daviquininooficial@gmail.com>
2025-03-13 15:11:43 -0300

    adiciona uma nova pagina de detalhes do livro

---
+++ details.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+    const currentAge = params.get("age");
+
+    const ageColors = {
+        "L": "age-livre",
+        "10": "age-10",
+        "12": "age-12",
+        "14": "age-14",
+        "16": "age-16",
+        "18": "age-18"
+    };
+    
+    document.querySelector(".book-title").textContent = params.get("title");
+    document.querySelector(".book-author").textContent = params.get("author");
+    document.querySelector(".book-cover").style.backgroundImage = `url(assets/capas/${params.get("image")})`;
+    document.querySelector(".book-description").textContent = params.get("desc");
+
+    const age = params.get("age");
+    const ageBadge = document.querySelector(".age-rating-two .age-badge-two");
+    ageBadge.textContent = age;
+    ageBadge.classList.add(`age-${age}`);
+}
+);
